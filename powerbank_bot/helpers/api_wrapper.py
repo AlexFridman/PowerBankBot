8dcae219ec0f7986b3c52ff6d454572f5025c592
Alexander Fridman <alexfridman@outlook.com>
2017-01-01 00:11:38 +0300

    Rename Credit to CreditType

--- powerbank_bot/helpers/api_wrapper.py
+++ powerbank_bot/helpers/api_wrapper.py
@@ -11,8 +11,8 @@ from powerbank_bot.config import Api
 User = namedtuple('User', ['user_id'])
 
 
-class Credit(
-    namedtuple('Credit', ['credit_id', 'name', 'description', 'currency', 'percent', 'overdue_percent', 'duration', ])):
+class CreditType(namedtuple('Credit', ['credit_id', 'name', 'description', 'currency', 'percent',
+                                       'overdue_percent', 'duration'])):
     def to_html(self):
         return td.HTML(('<b>{0.name}</b>\n'
                         'валюта: <i>{0.currency}</i>\n'
@@ -46,5 +46,5 @@ class ApiWrapper:
 
     def get_credits_info(self):
         url = os.path.join(Api.base_url, 'CreditTypes')
-        return [Credit.from_json(credit) for credit in requests.get(url, auth=Api.credentials).json()
+        return [CreditType.from_json(credit) for credit in requests.get(url, auth=Api.credentials).json()
                 if credit['IsActive']]
