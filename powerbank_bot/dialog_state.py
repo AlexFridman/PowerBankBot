55b758a2583985c566c44b70ec12bb181bb77788
Alexander Fridman <AlexFridman@outlook.com>
2016-12-31 18:09:30 +0300

    Implemented few DialogState methods

---
+++ powerbank_bot/dialog_state.py
@@ -0,0 +1,57 @@
+import logging
+
+from powerbank_bot.api_wrapper import ApiWrapper
+from powerbank_bot.storage import Storage
+
+LOGGER = logging.getLogger('dialog_state')
+
+INITIAL_DIALOG_STATE = {
+    'user_id': None,
+    'phone_number': None,
+    'is_authenticated': False,
+    'is_auth_locked': False
+}
+
+
+class DialogState:
+    def __init__(self, storage, api_wrapper, state):
+        self._state = state
+        self._storage = storage
+        self._api_wrapper = api_wrapper
+
+    @classmethod
+    def load(cls, dialog_id):
+        LOGGER.debug('Requested dialog ({}) state loading'.format(dialog_id))
+        storage = Storage()
+        api_wrapper = ApiWrapper()
+
+        dialog_state = storage.get_dialog_state(dialog_id)
+        if dialog_state is None:
+            LOGGER.debug('Failed to load state of dialog ({})'.format(dialog_id))
+            dialog_state = INITIAL_DIALOG_STATE.copy()
+            dialog_state['dialog_id'] = dialog_id
+
+        return cls(storage, api_wrapper, dialog_state)
+
+    def save(self):
+        self._storage.upsert_dialog_state(self._state)
+
+    @property
+    def is_authenticated(self):
+        return self._state['is_authenticated']
+
+    @property
+    def is_auth_blocked(self):
+        return self._state['is_auth_blocked']
+
+    @property
+    def dialog_id(self):
+        return self._state['dialog_id']
+
+    @property
+    def user_id(self):
+        return self._state['dialog_id']
+
+    @property
+    def phone_number(self):
+        return self._state['phone_number']
