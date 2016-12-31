927236165d2024dadeb0c866f59793e872483ae6
Alexander Fridman <AlexFridman@outlook.com>
2016-12-31 15:36:24 +0300

    Few changes

--- powerbank_bot/main.py
+++ powerbank_bot/main.py
@@ -24,19 +24,21 @@ class Menu:
 @td.requires_personal_chat('Идем в личный чат')
 def main_menu(start_message):
     user_id = start_message['id']
-    menu = Menu([MenuItem(info_menu(), 'Инфо')])
 
-    if Storage().is_authenticated_user(user_id):
-        menu.items.append(MenuItem(personal_info(), 'Личный кабинет'))
-    else:
-        menu.items.append(MenuItem(auth_dialog(user_id), 'Авторизоваться'))
+    while True:
+        menu = Menu([MenuItem(info_menu(user_id), 'Инфо')])
 
-    selected, msg = yield from td.require_choice('Привет!', menu.choose_list, 'Нужно что то выбрать')
+        if Storage().is_authenticated_user(user_id):
+            menu.items.append(MenuItem(personal_info(), 'Личный кабинет'))
+        else:
+            menu.items.append(MenuItem(auth_dialog(user_id), 'Авторизоваться'))
 
-    yield from menu.items[selected].coroutine
+        selected, msg = yield from td.require_choice('Главное меню', menu.choose_list, 'Нужно что то выбрать')
+
+        yield from menu.items[selected].coroutine
 
 
-def info_menu():
+def info_menu(user_id):
     credits = ApiWrapper().get_credits()
     credit_names = [c['name'] for c in credits]
 
@@ -47,43 +49,58 @@ def info_menu():
         if selected == 0:
             return
         else:
-            yield from credit_info(credits[selected - 1])
+            yield from credit_info(credits[selected - 1], Storage().is_authenticated_user(user_id))
 
 
 def auth_dialog(user_id):
-    phone_number = yield from text_question('Введите номер телефона (375 XX XXXXXX)', [PhoneNumberValidator()], True)
-    if not phone_number:
-        return
+    ENTER_PHONE_NUMBER_TRIALS_COUNT = 3
+    ENTER_VERIFICATION_CODE_TRIALS_COUNT = 3
 
-    is_user_exists = False
+    PHONE_NUMBER_MESSAGE = 'Введите номер телефона (375 XX XXXXXX)'
 
-    while not is_user_exists:
+    for _ in range(ENTER_PHONE_NUMBER_TRIALS_COUNT):
+        phone_number = yield from text_question(
+            PHONE_NUMBER_MESSAGE,
+            [PhoneNumberValidator()],
+            show_back_button=True)
+
+        if not phone_number:
+            return
+
         user = ApiWrapper().get_user_by_phone_number(phone_number)
+
         if user:
-            is_user_exists = True
+            break
         else:
-            phone_number = yield from text_question(
-                'Пользователь с таким номер не зарегистрирован\nВведите номер телефона',
-                [PhoneNumberValidator()], True)
-            if not phone_number:
-                return
+            PHONE_NUMBER_MESSAGE = 'Пользователь с таким номерорм не зарегистрирован\nВведите номер телефона'
+    else:
+        # TODO: блокировать на время
+        yield td.HTML('Кол-во попыток превышено. Блокировка'), ['Меню']
+        return
 
     verification_code = '1234'
     # send sms
 
-    code = yield from text_question('Введите проверочный код (1234)', show_back_button=True)
-    if not code:
-        return
+    VERIFICATION_CODE_MESSAGE = 'Введите проверочный код (1234)'
 
-    if code != verification_code:
-        is_correct_code = False
-        # TODO: limit tries
-        while not is_correct_code:
-            code = yield from text_question('Неверный код. Попробуйте еще раз', show_back_button=True)
-            if not code:
-                return
-            if code == verification_code:
-                is_correct_code = True
+    for _ in range(ENTER_VERIFICATION_CODE_TRIALS_COUNT):
+        code = yield from text_question(
+            VERIFICATION_CODE_MESSAGE,
+            show_back_button=True)
+
+        if not code:
+            return
+
+        if code == verification_code:
+            break
+        else:
+            VERIFICATION_CODE_MESSAGE = 'Неверный код. Попробуйте еще раз'
+    else:
+        # TODO: блокировать на время
+        # yield td.HTML('Кол-во попыток превышено. Блокировка'), ['Меню']
+        # return
+        yield from only_back('Кол-во попыток превышено. Блокировка')
+        return
 
     Storage().authenticate_user(user_id)
 
@@ -92,15 +109,23 @@ def personal_info():
     pass
 
 
-def only_back():
-    return td.require_choice('Только назад', [BACK_BUTTON_CONTENT], 'Только назад')
+def only_back(message='Только назад'):
+    return td.require_choice(message, [BACK_BUTTON_CONTENT], 'Только назад')
 
 
-def credit_info(credit):
-    answer = yield 'Кредит {name}\n{percent}%'.format(name=credit['name'], percent=credit['%']), [BACK_BUTTON_CONTENT]
-    if answer['text'] == BACK_BUTTON_CONTENT:
+def credit_info(credit, is_authenticated=False):
+    credit_info = 'Кредит {name}\n{percent}%'.format(name=credit['name'], percent=credit['%'])
+    button_list = [BACK_BUTTON_CONTENT]
+    if is_authenticated:
+        button_list.append('Подать заявку')
+    selected, _ = yield from td.require_choice(credit_info, button_list)
+
+    if selected == 0:
         return
-    yield from only_back()
+    elif selected == 1 and is_authenticated:
+        pass  # GO TO credit request form
+    else:
+        yield from only_back()
 
 
 if __name__ == '__main__':
