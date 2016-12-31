af29d765ade930b73ab27737faefe98c56126c76
Alexander Fridman <alexfridman@outlook.com>
2016-12-31 21:31:33 +0300

    Rewrite auth_dialog

--- powerbank_bot/bot/dialogs.py
+++ powerbank_bot/bot/dialogs.py
@@ -1,128 +1,117 @@
+import logging
+
 import telegram_dialog as td
+from collections import OrderedDict
 
-from powerbank_bot.bot.field_coroutines import BACK_BUTTON_CONTENT, text_question
+from powerbank_bot.bot.dialog_state import DialogState, UserNotFoundError
+from powerbank_bot.bot.field_coroutines import text_question, BACK_BUTTON_CONTENT
 from powerbank_bot.bot.validators import PhoneNumberValidator
-from powerbank_bot.helpers.api_wrapper import ApiWrapper
-from powerbank_bot.helpers.storage import Storage
 
-
-class MenuItem:
-    def __init__(self, coroutine, description):
-        self.coroutine = coroutine
-        self.description = description
+logging.basicConfig(level=logging.DEBUG)
 
 
 class Menu:
-    def __init__(self, items):
-        self.items = items
+    def __init__(self, items=None):
+        self.items = OrderedDict(enumerate(items or []))
 
-    @property
-    def choose_list(self):
-        return [item.description for item in self.items]
+    def add_item(self, dialog_procedure, display_name):
+        self.items[len(self.items)] = (dialog_procedure, display_name)
 
+    def __getitem__(self, index):
+        row, column = index
+        dialog_procedure, _ = self.items[row]
+        return dialog_procedure
 
-@td.requires_personal_chat('Идем в личный чат')
-def main_menu(start_message):
-    user_id = start_message['id']
-
-    while True:
-        menu = Menu([MenuItem(info_menu(user_id), 'Инфо')])
-
-        if Storage().is_authenticated_user(user_id):
-            menu.items.append(MenuItem(personal_info(), 'Личный кабинет'))
-        else:
-            menu.items.append(MenuItem(auth_dialog(user_id), 'Авторизоваться'))
-
-        selected, msg = yield from td.require_choice('Главное меню', menu.choose_list, 'Нужно что то выбрать')
-
-        yield from menu.items[selected].coroutine
+    def get_menu(self):
+        return [[display_name] for _, (_, display_name) in self.items.items()]
 
 
-def info_menu(user_id):
-    credits = ApiWrapper().get_credits()
-    credit_names = [c['name'] for c in credits]
+@td.requires_personal_chat('Работаю tet-a-tet')
+def main_menu_dialog(start_message):
+    dialog_id = start_message.chat_id
+    dialog_state = DialogState.load(dialog_id)
 
     while True:
-        (selected, _), msg = yield from td.require_choice('Тип кредита',
-                                                          [[BACK_BUTTON_CONTENT]] + [[c] for c in credit_names],
-                                                          'Нужно что то выбрать')
-        if selected == 0:
-            return
-        else:
-            yield from credit_info(credits[selected - 1], Storage().is_authenticated_user(user_id))
+        menu = Menu([(credit_list_dialog(dialog_state), 'Информация о доступных кредитах')])
 
+        if dialog_state.is_authenticated:
+            menu.add_item(personal_account_dialog(dialog_state), 'Личный кабинет')
+            menu.add_item(log_out_dialog(dialog_state), 'Выйти')
+        elif not dialog_state.is_auth_locked:
+            menu.add_item(auth_dialog(dialog_state), 'Авторизация')
 
-def auth_dialog(user_id):
-    ENTER_PHONE_NUMBER_TRIALS_COUNT = 3
-    ENTER_VERIFICATION_CODE_TRIALS_COUNT = 3
-
-    PHONE_NUMBER_MESSAGE = 'Введите номер телефона (375 XX XXXXXX)'
-
-    for _ in range(ENTER_PHONE_NUMBER_TRIALS_COUNT):
-        phone_number = yield from text_question(
-            PHONE_NUMBER_MESSAGE,
-            [PhoneNumberValidator()],
-            show_back_button=True)
-
-        if not phone_number:
-            return
-
-        user = ApiWrapper().get_user_by_phone_number(phone_number)
-
-        if user:
-            break
-        else:
-            PHONE_NUMBER_MESSAGE = 'Пользователь с таким номерорм не зарегистрирован\nВведите номер телефона'
-    else:
-        # TODO: блокировать на время
-        yield td.HTML('Кол-во попыток превышено. Блокировка'), ['Меню']
-        return
-
-    verification_code = '1234'
-    # send sms
-
-    VERIFICATION_CODE_MESSAGE = 'Введите проверочный код (1234)'
-
-    for _ in range(ENTER_VERIFICATION_CODE_TRIALS_COUNT):
-        code = yield from text_question(
-            VERIFICATION_CODE_MESSAGE,
-            show_back_button=True)
-
-        if not code:
-            return
-
-        if code == verification_code:
-            break
-        else:
-            VERIFICATION_CODE_MESSAGE = 'Неверный код. Попробуйте еще раз'
-    else:
-        # TODO: блокировать на время
-        # yield td.HTML('Кол-во попыток превышено. Блокировка'), ['Меню']
-        # return
-        yield from only_back('Кол-во попыток превышено. Блокировка')
-        return
-
-    Storage().authenticate_user(user_id)
-
-
-def personal_info():
-    pass
+        selected, _ = yield from td.require_choice('Главное меню', menu.get_menu(), 'Сделайте Ваш выбор')
+        yield from menu[selected]
 
 
 def only_back(message='Только назад'):
     return td.require_choice(message, [BACK_BUTTON_CONTENT], 'Только назад')
 
 
-def credit_info(credit, is_authenticated=False):
-    credit_info = 'Кредит {name}\n{percent}%'.format(name=credit['name'], percent=credit['%'])
-    button_list = [BACK_BUTTON_CONTENT]
-    if is_authenticated:
-        button_list.append('Подать заявку')
-    selected, _ = yield from td.require_choice(credit_info, button_list)
+def auth_dialog(dialog_state):
+    enter_phone_number_trials_count = 3
+    enter_verification_code_trials_count = 3
 
-    if selected == 0:
-        return
-    elif selected == 1 and is_authenticated:
-        pass  # GO TO credit request form
+    phone_number_message = 'Введите номер телефона (375 XX XXXXXX)'
+
+    for _ in range(enter_phone_number_trials_count):
+        phone_number = yield from text_question(
+            phone_number_message,
+            [PhoneNumberValidator()],
+            show_back_button=True
+        )
+
+        if not phone_number:
+            # "back" button pressed
+            return
+
+        try:
+            dialog_state.start_authentication(phone_number)
+        except UserNotFoundError:
+            phone_number_message = 'Пользователь с таким номерорм не зарегистрирован\nВведите номер телефона'
+        except Exception:
+            # TODO: call reset_auth_state
+            yield from only_back('Произошла ошибка. Попробуйте позже')
+            return
+        else:
+            break
     else:
-        yield from only_back()
+        dialog_state.lock_auth()
+        dialog_state.save()
+        yield 'Количество попыток превышено. Попробуйте позже', ['Меню']
+        return
+
+    verification_code_message = 'Введите проверочный код'
+
+    for _ in range(enter_verification_code_trials_count):
+        code = yield from text_question(
+            verification_code_message,
+            show_back_button=True
+        )
+
+        if not code:
+            # "back" button pressed
+            # TODO: call reset_auth_state
+            return
+
+        if dialog_state.complete_authentication(code):
+            break
+        else:
+            verification_code_message = 'Неверный код. Попробуйте еще раз'
+    else:
+        dialog_state.lock_auth()
+        dialog_state.save()
+        yield 'Количество попыток превышено. Попробуйте позже', ['Меню']
+        return
+
+
+def credit_list_dialog(dialog_state):
+    answer = yield 'Список кредитов'
+
+
+def log_out_dialog(dialog_state):
+    answer = yield 'Выход'
+
+
+def personal_account_dialog(dialog_state):
+    answer = yield 'Личный кабинет'
--- powerbank_bot/main.py
+++ powerbank_bot/main.py
@@ -1,8 +1,8 @@
-from powerbank_bot.bot.dialogs import main_menu
+from powerbank_bot.bot.dialogs import main_menu_dialog
 
 if __name__ == '__main__':
     TOKEN = '300227038:AAEqqG_KMPhuq-eydlsT94TMY8eY46WRhjE'
     import telegram_dialog as td
 
-    dialog_bot = td.DialogBot(TOKEN, main_menu)
+    dialog_bot = td.DialogBot(TOKEN, main_menu_dialog)
     dialog_bot.start()
