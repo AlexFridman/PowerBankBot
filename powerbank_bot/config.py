6106716324ae1e4efd078858858874c3bba8d448
Alexander Fridman <alexfridman@outlook.com>
2016-12-31 21:31:02 +0300

    Update mongo credentials

--- powerbank_bot/config.py
+++ powerbank_bot/config.py
@@ -6,8 +6,8 @@ class Mongo(object):
 
     __hosts = ('ds141368.mlab.com:41368',)
 
-    username = 'b2b'
-    password = 'b2b'
+    username = 'bot'
+    password = 'bot'
     host = 'mongodb://{}:{}@{}/{}'.format(username, password, ','.join(__hosts), db)
 
     read_preference = pymongo.ReadPreference.PRIMARY
