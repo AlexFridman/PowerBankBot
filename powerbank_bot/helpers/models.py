8b58b51a1422bdd5dfc007ab7de12f000d87082b
Alexander Fridman <alexfridman@outlook.com>
2017-01-01 14:17:18 +0300

    Move models into separate module

---
+++ powerbank_bot/helpers/models.py
@@ -0,0 +1,58 @@
+import datetime
+
+import humanize
+import telegram_dialog as td
+from collections import namedtuple
+from emoji import emojize
+
+User = namedtuple('User', ['user_id'])
+
+
+class CreditType(namedtuple('Credit', ['credit_id', 'name', 'description', 'currency', 'percent',
+                                       'overdue_percent', 'duration'])):
+    def to_html(self):
+        return td.HTML(('<b>{0.name}</b>\n'
+                        'валюта: <i>{0.currency}</i>\n'
+                        'процент: <i>{0.percent}%</i>\n'
+                        'срок: <i>{0.duration}</i>\n'
+                        '<pre>{0.description}</pre>').format(self))
+
+    @classmethod
+    def from_json(cls, json):
+        humanize.i18n.activate('ru_RU')
+        return cls(
+            credit_id=json['Id'],
+            name=json['Name'],
+            description=json['Description'],
+            currency=json['CurrencyShort'],
+            percent=round(json['Percent'] * 100),
+            overdue_percent=json['OverduePercent'],
+            duration=humanize.naturaldelta(datetime.timedelta(days=int(json['ReturnTerm'].split('.')[0])))
+        )
+
+
+class UserCredit(namedtuple('UserCredit', ['credit_type', 'is_closed', 'start_date', 'end_date', 'main_debt'])):
+    @classmethod
+    def from_json(cls, json):
+        humanize.i18n.activate('ru_RU')
+        return UserCredit(
+            credit_type=CreditType.from_json(json['CreditType']),
+            is_closed=json['IsClosed'],
+            start_date=humanize.naturaldate(datetime.datetime.strptime(json['FormattedStartDate'][:10], '%d.%m.%Y')),
+            end_date=humanize.naturaldate(datetime.datetime.strptime(json['FormattedEndDate'][:10], '%d.%m.%Y')),
+            main_debt=json['MainDebt']
+        )
+
+    @property
+    def name(self):
+        if self.is_closed:
+            return emojize(':white_check_mark: {0.credit_type.name}'.format(self), use_aliases=True)
+        return self.credit_type.name
+
+    def to_html(self):
+        # TODO: replace with real template string
+        return td.HTML(('<b>{0.name}</b>\n'
+                        'валюта: <i>{0.currency}</i>\n'
+                        'процент: <i>{0.percent}%</i>\n'
+                        'срок: <i>{0.duration}</i>\n'
+                        '<pre>{0.description}</pre>').format(self.credit_type))
--- powerbank_bot/helpers/storage.py
+++ powerbank_bot/helpers/storage.py
@@ -1,5 +1,7 @@
 import logging
 
+import pymongo
+
 from powerbank_bot.config import Mongo
 
 LOGGER = logging.getLogger('Storage')
@@ -24,3 +26,16 @@ class Storage:
             LOGGER.exception('Failed to upsert dialog ({}) state'.format(dialog_state['dialog_id']))
         else:
             LOGGER.exception('Dialog ({}) state upserted successfully'.format(dialog_state['dialog_id']))
+
+    def get_user_request_updates(self, user_id, limit=20):
+        try:
+            return list(self._db.updates.find({'user_id': user_id, 'seen': False}, limit=limit)
+                        .sort('timestamp', pymongo.DESCENDING))
+        except:
+            LOGGER.exception('Failed to retrieve user ({}) request updates'.format(user_id))
+
+    def mark_request_update_as_seen(self, update_id):
+        try:
+            self._db.updates.update_one({'update_id': update_id}, {'$set': {'seen': True}}, upsert=False)
+        except:
+            LOGGER.exception('Failed mark request update ({}) as seen'.format(update_id))
