270fd7c25ba1572682db662c7a3eb3e049a20799
Alexander Fridman <alexfridman@outlook.com>
2017-01-02 20:53:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-from distutils.core import setup
+import os
+
+from setuptools import setup
+
+
+def reqs(f):
+    return filter(None, (l.split('#', 1)[0].strip() for l in open(os.path.join(os.getcwd(), f))
+                         if not l.startswith('--')))
+
 
 setup(
     name='powerbank_bot',
     author='alfrid',
+    install_requires=reqs('requirements.txt')
 )
