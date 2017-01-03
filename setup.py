47f8ae56add029d313ab4f56860e4636b75ead05
Alexander Fridman <alexfridman@outlook.com>
2017-01-03 04:01:29 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def reqs(f):
@@ -12,6 +12,7 @@ setup(
     name='powerbank_bot',
     author='alfrid',
     install_requires=reqs('requirements.txt'),
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'run_bot=powerbank_bot.main:main'
